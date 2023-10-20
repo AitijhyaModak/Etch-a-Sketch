5c2cf571bae3d001a2a025cd8a3fed713fbe49b0
Aitijhya Modak <aitijhyasps1@gmail.com>
2023-10-20 21:43:35 +0530

    Complete the project

--- index.js
+++ index.js
@@ -1,25 +1,95 @@
-let drawing_box = document.querySelector("#box");
-drawing_box.style.height = "200px";
-drawing_box.style.width = "200px";
+let canvas = document.querySelector("#canvas");
+let grid_size = 64;
 
-let grid_size = 16;
-//creating the drawing box
-let created;
 
-for(let i=1;i<=256;i++)
-{   
-    created = document.createElement("div");
-    created.style.border = "solid green 1px";
-    created.style.width = "10.9px";
-    created.style.height = "10.9px";
-    created.classList.add("minibox");
-    created.addEventListener("click",onclick);
-    drawing_box.appendChild(created);
+
+//function for creating grids
+function creategrids() {
+    for (let i = 0; i < grid_size * grid_size; i++) {
+        let minibox = document.createElement("div");
+        /*minibox.style.border = "red solid 1px";*/
+        minibox.style.width = String((500 - 1.6 * grid_size) / grid_size) + "px";
+        minibox.style.height = String((500 - 1.6 * grid_size) / grid_size) + "px";
+        minibox.classList.add("minibox");
+        minibox.addEventListener("mouseover",draw);
+        canvas.appendChild(minibox);
+    }
+}
+creategrids();
+
+
+// grid size selection
+let grid_button = document.querySelector("#grid");
+grid_button.addEventListener("click", ongridclick);
+function ongridclick(event) {
+    for (let i = 0; i < grid_size * grid_size; i++) {
+        let minibox = canvas.querySelector("div");
+        canvas.removeChild(minibox);
+    }
+    grid_size = prompt("Enter grid size. \nMax limit is 200.\nExample: Enter 70 for a size of 70*70", "64");
+    creategrids();
 }
 
-function onclick(event)
-{   
-    console.log(event);
+
+//color selection
+let rainbow_pressed = false;
+let rainbow_button = document.querySelector("#rainbow");
+let color = "red";
+let red_colorbutton = document.querySelector("#red");
+red_colorbutton.addEventListener("click", oncolorclick);
+let green_colorbutton = document.querySelector("#green");
+green_colorbutton.addEventListener("click", oncolorclick);
+let blue_colorbutton = document.querySelector("#blue");
+blue_colorbutton.addEventListener("click", oncolorclick);
+let color_button_list = [red_colorbutton, green_colorbutton, blue_colorbutton, rainbow_button];
+
+function oncolorclick(event) {
+    for (let button of color_button_list) { button.classList.remove("pressed"); }
+    rainbow_pressed = false;
+    let colorbutton = event.srcElement;
+    colorbutton.classList.toggle("pressed");
+    if (colorbutton == red_colorbutton) color = "red";
+    else if (colorbutton == blue_colorbutton) color = "blue";
+    else color = "green";
+}
+
+
+
+rainbow_button.addEventListener("click", onrainbowclick)
+function onrainbowclick(event) {
+    for (let button of color_button_list) button.classList.remove("pressed");
+
+    event.srcElement.classList.toggle("pressed");
+    if (rainbow_pressed == true) rainbow_pressed = false;
+    else rainbow_pressed = true;
+}
+
+function getcolor() {
+    if (rainbow_pressed === false) return color;
+    else color = getrainbowcolor();
+    return color;
+}
+function getrainbowcolor() {
+    let colorlist = ["violet", "indigo", "blue", "red", "yellow", "orange", "green"]
+    let random_number = Math.floor(Math.random() * 7);
+    return colorlist[random_number];
+}
+
+
+//drawing part
+function draw(event) {
     let element = event.srcElement;
-    element.style.background_color = "blue";
+    element.style.background = getcolor();
+}
+
+
+
+//erase part
+let erase_button = document.querySelector("#erase");
+erase_button.addEventListener("click", eraseall);
+function eraseall() {
+    let miniboxes = document.querySelectorAll(".minibox");
+    for (let minibox of miniboxes) {
+        minibox.style.backgroundColor = "white";
+    }
 }
